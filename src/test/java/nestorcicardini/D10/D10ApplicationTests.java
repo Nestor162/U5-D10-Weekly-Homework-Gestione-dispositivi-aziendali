4cec6bc3da669567566c14e8ac42247b5daaf372
Nestor162 <nestorcicardini@gmail.com>
2023-06-09 09:29:54 +0200

    Impostazione iniziale del progetto

---
+++ src/test/java/nestorcicardini/D10/D10ApplicationTests.java
@@ -0,0 +1,13 @@
+package nestorcicardini.D10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
